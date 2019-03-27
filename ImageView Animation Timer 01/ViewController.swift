8c007c331df6e4aa45998a236b238c18e43044c1
dit000 <dit000@dit000.local>
2019-03-27 16:24:49 +0900

    ImageView Timer

--- ImageView Animation Timer 01/ViewController.swift
+++ ImageView Animation Timer 01/ViewController.swift
@@ -9,12 +9,45 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var alienImageViewe: UIImageView!
+    @IBOutlet weak var countLabel: UILabel!
+    var count = 1
+    var isAnimation = false
+    var myTimer = Timer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        alienImageViewe.image = UIImage(named:"Image1")
+        
     }
 
-
+    @IBAction func playBtnPressed(_ sender: Any) {
+        print("play Pressed")
+        myTimer = Timer.scheduledTimer(timeInterval: 0.5, target: self, selector: #selector(playAnimation), userInfo: nil, repeats: true)
+        //timer 호출
+    }
+    
+    @IBAction func pauseBtnPressed(_ sender: Any) {
+        print("pause Pressed")
+        myTimer.invalidate()
+    }
+    @IBAction func stopBtnPressed(_ sender: Any) {
+        print("stop Pressed")
+        count = 0
+        //countLabel.text = String(count)
+    }
+    @objc func playAnimation(){
+        
+        if count == 5{
+            count = 1
+        } else {
+            count+=1
+        }
+        
+        alienImageViewe.image = UIImage(named: "Image\(count)")
+        countLabel.text = String(count)
+        
+    }
 }
 
